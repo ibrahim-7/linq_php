d5eb4283476e56ad7b2d8e3063b7df1df6c42dee
Ibrahim <iaibrahimansari7@gmail.com>
2023-09-29 16:01:31 +0530

    60% comeplete

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function showConnectionCode(databaseType) {
     var connectionCode = '';
     switch (databaseType) {
         case 'MySQL':
-            connectionCode = `$mysqlPDO = new PDO("mysql:host=localhost;dbname=mydb", "username", "password");`;
+            connectionCode = `$mysqlPDO = new PDO("mysql:host=localhost;dbname=ecomm", "username", "password");`;
             break;
         case 'PostgreSQL':
-            connectionCode = `$postgresPDO = new PDO("pgsql:host=localhost;dbname=mydb", "username", "password");`;
+            connectionCode = `$postgresPDO = new PDO("pgsql:host=localhost;port=5432;dbname=ecomm;user=postgres;password=paasword");`;
             break;
         case 'MsAccess':
-            connectionCode = `$accessPDO = new PDO("odbc:DSN=MyAccessDSN");`;
+            connectionCode = `$accessPDO = new PDO("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/programming/ecomm.accdb");`;
             break;
         case 'MongoDb':
             connectionCode = `require 'vendor/autoload.php'; use MongoDB\Client;  $mongoClient = new Client("mongodb://localhost:27017");`;
